333d40546409627a2c147baf3faa4c155e546b38
MichealHogan <michealhogan1997@gmail.com>
2022-04-15 21:07:35 +0100

    Updated AddNewProperty
    Set up Firestore. Now adds property post data to the database.  Property Data is associated with the landlord UID from the firebase authentication module. Added Error handling for fields.

--- app/src/main/java/com/example/gaff/AddNewProperty.java
+++ app/src/main/java/com/example/gaff/AddNewProperty.java
@@ -2,8 +2,10 @@ package com.example.gaff;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,9 +13,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -27,6 +35,7 @@ public class AddNewProperty extends Fragment {
     private static final String ARG_PARAM2 = "param2";
     FirebaseDatabase database;
     FirebaseAuth mAuth;
+    FirebaseFirestore fStore;
     DatabaseReference myRef;
 
     // TODO: Rename and change types of parameters
@@ -34,6 +43,11 @@ public class AddNewProperty extends Fragment {
     private String mParam2;
     private EditText addressLine1, addressLine2, pricePerMonth;
     Button addProperty;
+
+    String user_id;
+    //private StorageReference storageReference;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore firebaseFirestore;
 
     public AddNewProperty() {
         // Required empty public constructor
@@ -64,9 +78,6 @@ public class AddNewProperty extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-
-
-
     }
 
     @Override
@@ -79,37 +90,49 @@ public class AddNewProperty extends Fragment {
         pricePerMonth = (EditText) rootView.findViewById(R.id.pricePerMonth);
         addProperty = (Button) rootView.findViewById(R.id.add_property_btn);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+        user_id = firebaseAuth.getCurrentUser().getUid();
+
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        //storageReference = FirebaseStorage.getInstance().getReference();
 
         addProperty.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                addProperty();
-                Toast.makeText(getActivity(), "Property added.",
-                        Toast.LENGTH_SHORT).show();
+
+                final String address1 = addressLine1.getText().toString();
+                final String address2 = addressLine2.getText().toString();
+                final String price = pricePerMonth.getText().toString();
+                //final int price = Integer.parseInt(pricePerMonth.getText().toString());
+                if(!TextUtils.isEmpty(address1) && !TextUtils.isEmpty(address2) && !TextUtils.isEmpty(price)){
+                    addProperty(address1,address2,price);
+                }else{
+                    Toast.makeText(getActivity(), "Fill all the fields", Toast.LENGTH_SHORT).show();
+                }
             }
         });
-
-
         return rootView;
     }
-    public boolean validPropertyInputs(String addressL1, String addressL2, int pricePerMonth){
+
+    public boolean validPropertyInputs(String addressL1, String addressL2, String pricePerMonth){
         return true;
     }
 
-    private void addProperty() {
-        if(validPropertyInputs(addressLine1.getText().toString(), addressLine2.getText().toString(),Integer.parseInt(pricePerMonth.getText().toString()))){
-
-            mAuth = FirebaseAuth.getInstance();
-            database = FirebaseDatabase.getInstance("https://gaff-14394-default-rtdb.firebaseio.com");
-            myRef = database.getReference("properties");
-            PropertyHelper propertyHelper = new PropertyHelper(addressLine1.getText().toString(), addressLine2.getText().toString(),Integer.parseInt(pricePerMonth.getText().toString()));
-
-            myRef.child(mAuth.getUid()).setValue(propertyHelper);
-            addressLine2.setText("");
-            addressLine1.setText("");
-            pricePerMonth.setText("");
-        }
+    private void addProperty(String addressLine1, String addressLine2, String pricePerMonth) {
+        Map<String, Object> property = new HashMap<>();
+        property.put("addressLine1", addressLine1);
+        property.put("addressLine2", addressLine2);
+        property.put("pricePerMonth", pricePerMonth);
+        firebaseFirestore.collection("Properties").document(user_id).set(property).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(getActivity(), "Property Successfully Added",
+                            Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getActivity(), "Firestore Error: "+task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
-
-
 }
